a9950f9635d033bafd418caacaa9dfbd9964059e
dward <devdave@ominian.net>
2011-05-24 16:38:49 -0600

    More minor fixes and tweaks to doctrine setup

---
+++ modules/doctrine2/config/doctrine.php
@@ -0,0 +1,7 @@
+<?php
+//Naming conventions for configuration file
+// This will eventually be enforced by Jenkins so try to obey them.
+return array(    
+    //Should the module bootstrap with SQL logging in place?
+    'log_sql' => false    
+)
